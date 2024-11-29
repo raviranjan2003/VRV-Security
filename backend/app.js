16202fa12c1cb0743c65b6a95c5c375df447f3be
Ravi <2140131@sliet.ac.in>
2024-11-29 19:37:47 +0530

    cors permitted to client app

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000','https://vrv-security-frontend.onrender.com'],
     credentials: true,                
 }));
 
